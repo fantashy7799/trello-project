bc537425fad1148e23276aed82a44617ebdc96d2
fantashy7799 <fantashy7799@gmail.com>
2022-04-26 02:21:06 +0300

    Clean react Architecher

--- src/App.js
+++ src/App.js
@@ -1,124 +1,19 @@
+import React from "react";
 import './App.scss'
+
+
+//Custom components
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from "components/BoardContent/BoardContent";
+
 
 function App() {
   return (
     <div className="trello-master">
-      <nav className='navbar app'>App Bar</nav>
-      <nav className='navbar board'>Board Bar</nav>
-      <div className='board-columns'>
-        <div className='column'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src='1.png'
-            alt='nguyenuydinh-alt-img'/>
-            Title: NGUYENUYDINH
-          </li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-        </ul>
-        <footer>Add another card</footer>
-        </div> 
-
-        <div className='column'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src='1.png'
-            alt='nguyenuydinh-alt-img'/>
-            Title: NGUYENUYDINH
-          </li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-        </ul>
-        <footer>Add another card</footer>
-        </div> 
-
-        <div className='column'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src='1.png'
-            alt='nguyenuydinh-alt-img'/>
-            Title: NGUYENUYDINH
-          </li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-        </ul>
-        <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src='1.png'
-            alt='nguyenuydinh-alt-img'/>
-            Title: NGUYENUYDINH
-          </li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-        </ul>
-        <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src='1.png'
-            alt='nguyenuydinh-alt-img'/>
-            Title: NGUYENUYDINH
-          </li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-        </ul>
-        <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src='1.png'
-            alt='nguyenuydinh-alt-img'/>
-            Title: NGUYENUYDINH
-          </li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-        </ul>
-        <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img src='1.png'
-            alt='nguyenuydinh-alt-img'/>
-            Title: NGUYENUYDINH
-          </li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-          <li>Add what you'd like to work on below</li>
-        </ul>
-        <footer>Add another card</footer>
-        </div>
-
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './Task.scss'
+
+function Task() {
+    return(
+        <li className="task-item">
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+        </li>
+    )
+}
+
+export default Task
