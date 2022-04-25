170233ac899fe4ca9da4528e235c76cf63795543
fantashy7799 <fantashy7799@gmail.com>
2022-04-26 00:20:49 +0300

    design trello frontend.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,124 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hello-world">
-        Hello world
-      </h1>
+    <div className="trello-master">
+      <nav className='navbar app'>App Bar</nav>
+      <nav className='navbar board'>Board Bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div> 
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div> 
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src='1.png'
+            alt='nguyenuydinh-alt-img'/>
+            Title: NGUYENUYDINH
+          </li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+      </div>
     </div>
   );
 }
