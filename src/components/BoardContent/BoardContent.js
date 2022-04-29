2937aff707ee3a23de012e565dd98e8d47d3afff
fantashy7799 <fantashy7799@gmail.com>
2022-04-29 22:10:55 +0300

    update and remove column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -97,6 +97,29 @@ function BoardContent() {
     toggleOpenNewColumnForm()
   }
 
+  const onUpdateColumn = (newColumnToUpdate) => {
+    const columnIdToUpdate = newColumnToUpdate.id
+
+    let newColumns = [...columns]
+
+    const columnIndexToUpdate = newColumns.findIndex(i => i.id ===columnIdToUpdate)
+
+    if (newColumnToUpdate._destroy) {
+      //remove column
+      newColumns.splice(columnIndexToUpdate, 1)
+    } else {
+      //update column info
+      newColumns.splice(columnIndexToUpdate, 1, newColumnToUpdate)
+    }
+
+    let newBoard = { ...board }
+    newBoard.columOrder = newColumns.map(c => c.id)
+    newBoard.columns = newColumns
+
+    setColumns(newColumns)
+    setBoard(newBoard)
+  }
+
   return (
     <div className='board-content'>
       <Container
@@ -113,7 +136,7 @@ function BoardContent() {
       >
         {columns.map((column, index) => (
           <Draggable key={index}>
-            <Column column={column} onCardDrop={onCardDrop} />
+            <Column column={column} onCardDrop={onCardDrop} onUpdateColumn={onUpdateColumn} />
           </Draggable>
         ))}
       </Container>
